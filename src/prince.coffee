f51124293377a7466c9788794c4f7bc2e4cc32a4
Paul Pflugradt <paulpflugradt@googlemail.com>
2014-06-13 10:13:13 +0200

    bugfix

--- src/prince.coffee
+++ src/prince.coffee
@@ -1,18 +1,17 @@
 module.exports = (grunt) ->
-  
-helper = require("./lib/prince-lib").init(grunt)
-grunt.registerMultiTask "prince", "Your task description goes here.", ->
-  @files.forEach (f) ->
-    f.src.forEach (src) ->
-      grunt.log.writeln "pdf output is: " + f.dest
-      
-      helper.convert
-        code: 90
-        args: [
-          "--media=print" # Use @print media type
-          src
-          "--output"
-          f.dest
-        ]
-        done: (err) ->
-          grunt.log ">>>", err  if err
+  helper = require("./lib/prince-lib").init(grunt)
+  grunt.registerMultiTask "prince", "Your task description goes here.", ->
+    @files.forEach (f) ->
+      f.src.forEach (src) ->
+        grunt.log.writeln "pdf output is: " + f.dest
+        
+        helper.convert
+          code: 90
+          args: [
+            "--media=print" # Use @print media type
+            src
+            "--output"
+            f.dest
+          ]
+          done: (err) ->
+            grunt.log ">>>", err  if err
--- tasks/prince.js
+++ tasks/prince.js
@@ -1,25 +1,23 @@
 (function() {
-  var helper;
-
-  module.exports = function(grunt) {};
-
-  helper = require("./lib/prince-lib").init(grunt);
-
-  grunt.registerMultiTask("prince", "Your task description goes here.", function() {
-    return this.files.forEach(function(f) {
-      return f.src.forEach(function(src) {
-        grunt.log.writeln("pdf output is: " + f.dest);
-        return helper.convert({
-          code: 90,
-          args: ["--media=print", src, "--output", f.dest],
-          done: function(err) {
-            if (err) {
-              return grunt.log(">>>", err);
+  module.exports = function(grunt) {
+    var helper;
+    helper = require("./lib/prince-lib").init(grunt);
+    return grunt.registerMultiTask("prince", "Your task description goes here.", function() {
+      return this.files.forEach(function(f) {
+        return f.src.forEach(function(src) {
+          grunt.log.writeln("pdf output is: " + f.dest);
+          return helper.convert({
+            code: 90,
+            args: ["--media=print", src, "--output", f.dest],
+            done: function(err) {
+              if (err) {
+                return grunt.log(">>>", err);
+              }
             }
-          }
+          });
         });
       });
     });
-  });
+  };
 
 }).call(this);
